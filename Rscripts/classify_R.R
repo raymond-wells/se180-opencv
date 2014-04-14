e2f0f0c9f79fb848d256a12ce254160b4f03cbac
Your Name <you@example.com>
2014-04-14 14:23:05 -0700

    Updated classifier.

--- Rscripts/classify_R.R
+++ Rscripts/classify_R.R
@@ -1,5 +1,6 @@
 library(randomForest)
-input_file = commandArgs()[length(commandArgs())]
+input_file = commandArgs()[length(commandArgs())-1]
+algo = commandArgs()[length(commandArgs())]
 inp <- read.csv(input_file, header=FALSE)
-load("data/ts.rfmodel.rda")
+load(paste("data/ts.rfmodel",algo,".rda"))
 predict(ts.rfmodel, inp)
